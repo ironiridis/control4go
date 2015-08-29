5c5e61f897feb62ace7becf908c0f9727b805010
Chris Harrington <ironiridis@gmail.com>
2015-08-28 22:25:33 -0500

    expand stubs (and don't be dumb and reference CIP)

--- amx/packets.go
+++ amx/packets.go
@@ -1,10 +1,33 @@
 package icsp
 
+import "fmt"
+
+// Packet is an interface for getting the information out of an ICSP Packet.
+type Packet interface {
+	Parse() error
+	String() string
+}
+
 type PacketParser struct{}
-type Packet struct{}
+type RawPacket struct {
+	raw           []byte
+	payloadLength int
+}
 
 func (p *PacketParser) Write(b []byte) (int, error) { return 0, nil }
 func (p *PacketParser) Parse() int                  { return 0 }
-func (p *Packet) String() string                    { return "" }
 
 func NewPacketParser() (*PacketParser, chan Packet) { return nil, nil }
+
+// RawPayload returns the byte data of the packet
+func (p *RawPacket) RawPayload() []byte {
+	if p.payloadLength == 0 {
+		return nil
+	}
+	return p.raw
+}
+
+// String returns a dump of the packet data
+func (p *RawPacket) String() string {
+	return fmt.Sprintf("raw=% 2x", p.RawPayload())
+}
