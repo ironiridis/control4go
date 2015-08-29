bbd82e7a6e21ffecf189269e2141edbe735fcd91
Chris Harrington <ironiridis@gmail.com>
2015-08-28 21:46:27 -0500

    fix stub to match cip

--- amx/packets.go
+++ amx/packets.go
@@ -4,7 +4,7 @@ type PacketParser struct{}
 type Packet struct{}
 
 func (p *PacketParser) Write(b []byte) (int, error) { return 0, nil }
-func (p *PacketParser) Parse()                      {}
+func (p *PacketParser) Parse() int                  { return 0 }
 func (p *Packet) String() string                    { return "" }
 
 func NewPacketParser() (*PacketParser, chan Packet) { return nil, nil }
